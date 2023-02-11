0dc36277d93de9611aa0488a69def5449c2d32f9
Sarwasvi <sarwasviingoley@gmail.com>
2023-02-11 13:21:59 +0530

    first commit

---
+++ ICart/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render, redirect
+
+def BASE(request):
+    return render(request, 'Main/base.html')
+
+def HOME(request):
+    return render(request, 'Main/index.html')
---
+++ ICartApp/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class IcartappConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'ICartApp'
